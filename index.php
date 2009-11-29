fa808e5cae2539e2b72a9f2dbdd983d6f818f2a3
John O'Connor <sax1johno@gmail.com>
2009-11-28 23:03:13 -0800

    Initial Commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>Punch in the throat</title>
+<meta name="punch in the throat" />
+<meta description="Punch someone in the throat today. You won't regret it, and you won't go to jail!" />
+<meta tags="Punch throat someone pain boss coworkers people stress relief hemmerhoids collect-callers" />
+</head>
+<body>
+<?php 
+if ($_GET['name'])
+{
+	?><p><? echo $_GET['name']?> needs to be punched in the throat.</p>
+
+<p>Did you think that was awesome?  Well, you're a dick.  Punch someone else in the throat.</p>
+<? }
+else {
+?><p>YOU need to be punched in the throat.  Provide a name, asshole!</p>
+<? }
+?>
+<form action="" method="get">
+<input type="text" name="name"></input>
+<input type="submit" value="Punch this person in the throat"></input>
+</form>
+</body>
+</html>
